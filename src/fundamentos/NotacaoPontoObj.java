3336eb28fc45b1e1865a63e64e1966036f429e08
Tomas <tomasd@grupodimed.com.br>
2021-10-20 09:52:09 -0300

    Second Day learning java

--- src/fundamentos/NotacaoPontoObj.java
+++ src/fundamentos/NotacaoPontoObj.java
@@ -1,2 +1,35 @@
-package fundamentos;public class NotacaoPontoObj {
+package fundamentos;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+
+public class NotacaoPontoObj {
+
+    public static void main(String[] args) {
+        //tipos primitivos não tem notação ponto.
+        String a = "a";
+        Double b= 4.0;
+        Integer c = 4;
+
+
+
+        System.out.println(a);
+        a = a.toUpperCase(Locale.ROOT);
+        System.out.println(a);
+
+        a= a.replace("A","b");
+        System.out.println(a);
+
+        System.out.println("Car".toUpperCase(Locale.ROOT).replace("CAR","Bus").concat("car"));
+
+
+        b.equals(a);
+
+        System.out.println(b);
+
+
+
+    }
+
+
 }
--- src/fundamentos/Temperatura.java
+++ src/fundamentos/Temperatura.java
@@ -1,2 +1,24 @@
-package fundamentos;public class Temperatura {
+package fundamentos;
+
+public class Temperatura {
+
+
+
+    public static void main(String[] args) {
+
+        // f - 32 ) x 5/9 = c
+
+        final double fator =5.0/9.0;
+        final double ajuste= 32;
+
+        double fahrenheit =86;
+        double celcius = (fahrenheit - ajuste);
+        System.out.println("Resultado " + celcius);
+
+        fahrenheit = 0;
+        celcius = (fahrenheit-ajuste) * fator;
+        System.out.println("resultado "+ celcius);
+
+    }
+
 }
